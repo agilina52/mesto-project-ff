56a74e5ab2d19cb40c1e7c18d1dafa9875eba01f
agilina52 <agilina52@yandex.ru>
2025-05-08 12:30:40 +0300

    edit

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,27 @@
 const cardTemplate = document.querySelector("#card-template").content;
 const cardList = document.querySelector(".places__list");
 
-function createCard(element) {
+function createCard(element, deleteCard) {
   const cardElement = cardTemplate.cloneNode(true);
   const deleteButton = cardElement.querySelector(".card__delete-button");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
 
-  cardElement.querySelector(".card__image").src = element.link;
-  cardElement.querySelector(".card__title").textContent = element.name;
+  cardImage.src = element.link;
+  cardImage.alt = `Изображение места: ${element.name}`;
+  cardTitle.textContent = element.name;
 
-  deleteButton.addEventListener("click", function () {
-    const cardDelete = deleteButton.closest(".card");
-    cardDelete.remove();
-  });
-
+  deleteButton.addEventListener("click", deleteCard);
   return cardElement;
 }
 
+function removeCard(event) {
+  const deleteButton = event.currentTarget;
+  const cardDelete = deleteButton.closest(".card");
+  cardDelete.remove();
+}
+
 initialCards.forEach(function (element) {
-  const cardElement = createCard(element);
+  const cardElement = createCard(element, removeCard);
   cardList.append(cardElement);
 });
